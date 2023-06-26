89b8e207ed55700e4864d9bd29e5d81d3b39a85b
Dmitri Volski <lipov758@gmail.com>
2023-06-27 00:32:44 +0200

    Add selenium tests for app

---
+++ Tests/SeleniumTests/SeleniumTests/TestCases/WeatherApp/HiglightsElementsTest.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+
+namespace SeleniumTests.TestCases.WeatherApp
+{
+	internal class HiglightsElementsTest
+	{
+		IWebDriver driver = new ChromeDriver();
+
+		[SetUp]
+		public void SetUp()
+		{
+			driver.Navigate().GoToUrl("http://127.0.0.1:5500/index.html");
+			driver.Manage().Window.Maximize();
+			Thread.Sleep(2000);
+		}
+
+		[Test]
+		public void WeekCaroosel()
+		{
+			IEnumerable<IWebElement> highligthElems = driver.FindElements(By.ClassName("highliteBox"));
+			Console.WriteLine($"ighlith elements count: {highligthElems.Count()}");
+			Assert.IsTrue(highligthElems.Count() == 6);
+		}
+
+		[TearDown]
+		public void TearDown()
+		{
+			driver.Close();
+		}
+	}
+}
---
+++ Tests/SeleniumTests/SeleniumTests/TestCases/WeatherApp/WeekCarooselTest.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+
+namespace SeleniumTests.TestCases.WeatherApp
+{
+	internal class WeekCarooselTest
+	{
+		IWebDriver driver = new ChromeDriver();
+
+		[SetUp]
+		public void SetUp()
+		{
+			driver.Navigate().GoToUrl("http://127.0.0.1:5500/index.html");
+			driver.Manage().Window.Maximize();
+			Thread.Sleep(2000);
+		}
+
+		[Test]
+		public void WeekCaroosel()
+		{
+			IEnumerable<IWebElement> caroseelElements = driver.FindElements(By.ClassName("oneDayWeatherBox"));
+			Console.WriteLine($"Elements count: {caroseelElements.Count()}");
+			Assert.IsTrue(caroseelElements.Count() == 7);
+		}
+
+		[TearDown]
+		public void TearDown()
+		{
+			driver.Close();
+		}
+	}
+}
